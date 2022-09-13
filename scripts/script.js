f15c5f03a9fe79e7763d69e8ae4fa1d4499b1ab8
Max Anisimov <makc.anisimov777@yandex.ru>
2022-09-14 00:50:27 +0300

    пр4 v 1.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
-const overlayElement = document.querySelector('.overlay');
+const popupElement = document.querySelector('.popup');
 const openPopupButton = document.querySelector('.profile__edit');
-const closePopupButton = overlayElement.querySelector('.popup__close-button');
-const savePopupButton = overlayElement.querySelector('.popup__save-button');
+const closePopupButton = popupElement.querySelector('.popup__close-button');
+const savePopupButton = popupElement.querySelector('.popup__save-button');
 
 var profileName = document.querySelector('.profile__name');
 var profileJob = document.querySelector('.profile__job');
-var profileEditName = overlayElement.querySelector('.popup__name');
-var profileEditJob = overlayElement.querySelector('.popup__job');
+var profileEditName = popupElement.querySelector('.popup__input-name');
+var profileEditJob = popupElement.querySelector('.popup__input-job');
 
 // const toggleOverlay = () => {
 //     popupElement.classList.toggle('overlay_opened');
 // }
-function openOverlay () {
-    overlayElement.classList.add('overlay_opened');
+function openPopup () {
+    popupElement.classList.add('popup_opened');
 }
 
-function closeOverlay () {
-    overlayElement.classList.remove('overlay_opened');
+function closePopup () {
+    popupElement.classList.remove('popup_opened');
 }
 
 
@@ -26,20 +26,20 @@ function fillEditForm() {
 };
 
 openPopupButton.addEventListener('click', () => {
-    openOverlay();
+    openPopup();
     fillEditForm();
 })
 
 closePopupButton.addEventListener ('click', () => {
-    closeOverlay();
+    closePopup();
 })
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = profileEditName.value;
-    console.log(profileEditName.value);
+    // console.log(profileEditName.value);
     profileJob.textContent = profileEditJob.value;
-    closeOverlay();
+    closePopup();
     };
 
-savePopupButton.addEventListener('submit', formSubmitHandler);
+savePopupButton.addEventListener('click', formSubmitHandler);
