5f3ed9f2dff517ca126c1df25f55161c3d9cf22e
Max Anisimov <makc.anisimov777@yandex.ru>
2022-09-27 22:35:26 +0300

    создать из template шаблона карточку

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,7 @@ const buttonClosePopupWiewPhoto = popupElementWievPhoto.querySelector('.popup__c
 
 
 
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let profileEditName = popupElementEditProfile.querySelector('#inputName');
-let profileEditJob = popupElementEditProfile.querySelector('#inputJob');
+
 
 function openPopup(popupWindow) {
   popupWindow.classList.add('popup_opened');
@@ -33,6 +30,7 @@ function fillProfileEditForm() {
 };
 
 //редактироване профиля---------------------------
+
 buttonProfileEdit.addEventListener('click', () => {
   openPopup(popupElementEditProfile);
   fillProfileEditForm();
@@ -40,6 +38,11 @@ buttonProfileEdit.addEventListener('click', () => {
 buttonClosePopupEditProfile.addEventListener('click', () => {
   closePopup(popupElementEditProfile);
 })
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let profileEditName = popupElementEditProfile.querySelector('#inputName');
+let profileEditJob = popupElementEditProfile.querySelector('#inputJob');
 
 function formSubmitEditProfile(evt) {
   evt.preventDefault();
@@ -49,42 +52,75 @@ function formSubmitEditProfile(evt) {
 };
 popupElementEditProfile.addEventListener('submit', formSubmitEditProfile);
 //--------------------------------------------------
-// !!!!добавление места
+
+// !!!!добавление места черновик
 buttonAddPhoto.addEventListener('click', () => {
   openPopup(popupElementAddPhoto);
-
 })
+
 buttonClosePopupAddPhoto.addEventListener('click', () => {
   closePopup(popupElementAddPhoto);
 })
+
+const elementsList = document.querySelectorAll('.element'); //NodeList всех карточек
+const templateElement = document.querySelector('.element-template').content; // шаблон карточки
+
+const inputAddPhotoName = popupElementAddPhoto.querySelector('#inputMestoName'); //инпут названия в попапе добавления фото
+const inputAddPhotoSrcLink = popupElementAddPhoto.querySelector('#inputMestoLink'); // инпут ссылки на фото в попапе добавления фото
+
+const items = Array.from(elementsList); //получили массив из NodeList всех элементов массива
+
+function formSubmitAddPhoto(evt) {
+  // evt.preventDefault();
+  //  = inputAddPhotoName.value;
+  //  = inputAddPhotoSrcLink.value;
+  console.log(inputAddPhotoName.value);
+  console.log(inputAddPhotoSrcLink.value);
+  // closePopup(popupElementAddPhoto);
+};
+popupElementAddPhoto.addEventListener('submit', formSubmitAddPhoto);
 //------------
 
-// реализация лайка на фотографиях
+
+
+//----------реализация лайка на фотографиях-----------------
 const elementsLike = document.querySelectorAll('.element__like'); //ищем все элементы имеющие кнопку like
 elementsLike.forEach(function (element) {                          //для каждого элемента
   element.addEventListener('click', function (like) {              //навешиваем слушатель: по клику выполняем
     like.target.classList.toggle('element__like_active');         //вкл-выкл лайка
   })
 });
-//-----------------------------
-// реализация просмотра фото по клику
+//-----------------------------------------------------------
+
+
+//-----------------реализация просмотра фото по клику----------
 const photoOpened = popupElementWievPhoto.querySelector('.popup__photo-opened');
 const photoTitle = popupElementWievPhoto.querySelector('.popup__photo-title');
 const elementsPhoto = document.querySelectorAll('.element__photo');  //ищем все элементы -фотографии
 elementsPhoto.forEach(function (element) {                          //для каждого элемента
   element.addEventListener('click', function () {
-    openPopup(popupElementWievPhoto);
-    const currentCard = element.parentElement; //получили всю карточку по которой клик на фото
+    const currentCard = element.closest('.element'); //получили всю карточку по которой клик на фото
     const currentCardTitle = currentCard.querySelector('.element__title'); //получили название фото
 
     photoOpened.src = element.src; //передаем в попап адрес фото
     photoOpened.alt = currentCardTitle.textContent; //передать в попапе альт для фото из названия карточки
     photoTitle.textContent = currentCardTitle.textContent; //передать в попап заголовок из названия фото
+    openPopup(popupElementWievPhoto);
   })
 });
 buttonClosePopupWiewPhoto.addEventListener('click', () => {
   closePopup(popupElementWievPhoto);
 })
-//----------------------------------------
+//--------------------------------------------------------------
 
-
+//-----------------------функция создания карточки--------------
+function createCard(dataCard) {
+  const newHtmlElement = templateElement.cloneNode(true); // из темплейта создаем шаблон карточки.
+  const  cardPhoto = newHtmlElement.querySelector('.element__photo'); //фото в карточке
+  const cardTitle = newHtmlElement.querySelector('.element__title');  //название места
+  cardTitle.textContent = dataCard.name;
+  cardPhoto.src = dataCard.link;
+  cardPhoto.alt = dataCard.name;
+  // return newHtmlElement;   надо ли чтото возвращать???
+};
+// ---------------------------------------------------------------
