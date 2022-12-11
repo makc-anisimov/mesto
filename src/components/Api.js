6419052b4bd0c4c16c35a46f25a6a615f02b3a4e
Max Anisimov <makc.anisimov777@yandex.ru>
2022-12-12 00:15:39 +0300

    пр9 сделал верстку редактирования аватара

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,15 +3,44 @@ class Api {
       // тело конструктора
     }
 
+    getProfile() {
+
+    }
+
+
     getInitialCards() {
       // ...
     }
 
+    edtiProfile() {
+
+    }
+
+    addCard() {
+
+    }
+
+    getLikes() {
+
+    }
+
+    deleteCard() {
+
+    }
+
+    addLike() {
+
+    }
+
+    deleteLike() {
+
+
+    }
     // другие методы работы с API
   }
 
-  const api = new Api({
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
+  export const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-55',
     headers: {
       authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
       'Content-Type': 'application/json'
